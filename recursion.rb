deab8a1029b7dcceebb03575571d933864bda82f
Adam Hill <adam.graham.hill@gmail.com>
2016-09-18 10:20:09 -0400

    Solve collatz conjecture

--- recursion.rb
+++ recursion.rb
@@ -22,5 +22,51 @@ def sum(n)
 	n == 0 ? 0 : n + sum(n -1)
 end
 
-puts factorial(4) == 24
-puts sum(4) == 10
+# puts factorial(4) == 24
+# puts sum(4) == 10
+
+# def fib_seq(n) 
+# 	fib_vals = [0, 1]
+# 	if n == 1 || n == 2
+# 		fib_vals[n - 1]
+# 	else
+# 		fib_vals[n - 1] = fib_seq(n - 2) + fib_seq(n - 1)
+# 	end
+# end
+
+def fib_seq(n)
+	if n == 1
+		0
+	elsif n == 2
+		1
+	else
+		fib_seq(n -2) + fib_seq(n - 1)
+	end
+end
+
+# puts fib_seq(1) == 0
+# puts fib_seq(2) == 1
+# puts fib_seq(3) == 1
+# puts fib_seq(4) == 2
+# puts fib_seq(5) == 3
+# puts fib_seq(6) == 5
+
+def collatz(n)  #calculate number of operations to get to 1
+	if n == 1
+		0
+	elsif n % 2 == 0
+		1 + collatz(n / 2)
+	else
+		1 + collatz(3 * n + 1)
+	end
+end
+
+puts collatz(1) #== 0
+puts collatz(2) #== 1
+puts collatz(3) #== 7
+puts collatz(27) #== 111
+
+
+
+
+
