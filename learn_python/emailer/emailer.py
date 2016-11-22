659cad324bd96f693abb45ae9684ab9b7341ec33
Adam Hill <adam.graham.hill@gmail.com>
2016-11-22 10:06:52 -0500

    Refactor collecting emails from file and schedules from file into their own respective functions. Also add main func

--- learn_python/emailer/emailer.py
+++ learn_python/emailer/emailer.py
@@ -1,21 +1,32 @@
-emails = {}
+def get_emails():
+    emails = {}
+    
+    try:
+        email_file = open('emails2.txt', 'r')
+    
+        for line in email_file:
+            (email, name) = line.split(',')
+            emails[email] = name.strip()
+    except FileNotFoundError as err:
+        print(err)
 
-try:
-    email_file = open('emails2.txt', 'r')
+    return emails
 
-    for line in email_file:
-        (email, name) = line.split(',')
-        emails[email] = name.strip()
-except FileNotFoundError as err:
-    print(err)
+def get_schedule():
+    try:
+        schedule_file = open('schedule.txt', 'r')
+    
+        schedule = schedule_file.read()
+    except FileNotFoundError as err:
+        print(err)
+    
+    return schedule
 
-print(emails)
+def main():
+    emails = get_emails()
+    print(emails)
 
-try:
-    schedule_file = open('schedule.txt', 'r')
+    schedule = get_schedule()
+    print(schedule)
 
-    schedule = schedule_file.read()
-except FileNotFoundError as err:
-    print(err)
-
-print(schedule)
+main()
